f319a4d17a43c2210c930c66ea4a5214aa52dfd3
Denis <denisdemaj233@gmail>
2024-08-10 14:08:41 +0200

    Ndryshimet per te perdorur JPAnn

--- src/main/java/Hibernate_Utils/Hibernate_Utils.java
+++ src/main/java/Hibernate_Utils/Hibernate_Utils.java
@@ -10,34 +10,34 @@ import org.hibernate.service.ServiceRegistry;
 import java.util.Properties;
 
 public class Hibernate_Utils {
-    @Getter
-    private static SessionFactory sessionFactory = createAndGetSessionFactory();
-    private static SessionFactory createAndGetSessionFactory() {
-        try{
-
-            Properties properties = new Properties();
-            properties.load(Hibernate_Utils.class
-                    .getClassLoader()
-                    .getResourceAsStream("hibernate.properties"));
-
-
-            Configuration configuration = new Configuration();
-            configuration.addProperties(properties);
-
-           // configuration.addAnnotatedClass(Student.class);
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-
-
-            return configuration.buildSessionFactory(serviceRegistry);
-        }catch (Throwable exception){
-            throw new RuntimeException(exception);
-        }
-    }
-
-    public static void setSessionFactory(SessionFactory sessionFactory) {
-        Hibernate_Utils.sessionFactory = sessionFactory;
-    }
+//    @Getter
+//    private static SessionFactory sessionFactory = createAndGetSessionFactory();
+//    private static SessionFactory createAndGetSessionFactory() {
+//        try{
+//
+//            Properties properties = new Properties();
+//            properties.load(Hibernate_Utils.class
+//                    .getClassLoader()
+//                    .getResourceAsStream("hibernate.properties"));
+//
+//
+//            Configuration configuration = new Configuration();
+//            configuration.addProperties(properties);
+//
+//           // configuration.addAnnotatedClass(Student.class);
+//
+//            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                    .applySettings(configuration.getProperties()).build();
+//
+//
+//            return configuration.buildSessionFactory(serviceRegistry);
+//        }catch (Throwable exception){
+//            throw new RuntimeException(exception);
+//        }
+//    }
+//
+//    public static void setSessionFactory(SessionFactory sessionFactory) {
+//        Hibernate_Utils.sessionFactory = sessionFactory;
+//    }
 }
 
