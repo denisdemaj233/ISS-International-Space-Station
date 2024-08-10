8d0055b5b43df3a8498a3828a6d07180569d5f3d
Denis <denisdemaj233@gmail>
2024-08-10 14:59:57 +0200

    Konfigurimi JPA i ri

---
+++ src/main/java/config/Jpa.java
@@ -0,0 +1,34 @@
+package config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.transaction.Transaction;
+
+public class Jpa {
+    private static final EntityManagerFactory entityManagerFactory = buildEntityFactoryManager();
+
+    private static EntityManagerFactory buildEntityFactoryManager() {
+
+        try {
+            return Persistence.createEntityManagerFactory("pu");
+        }catch (Exception e){
+           throw new ExceptionInInitializerError(e);
+        }
+
+    }
+
+    public static EntityManager getEntityManager(){
+
+        return entityManagerFactory.createEntityManager();
+
+    }
+
+    public static void shutdown(){
+        if(entityManagerFactory != null && entityManagerFactory.isOpen()){
+                entityManagerFactory.close();
+        }
+    }
+
+
+}
---
+++ src/main/java/org/hibernate/SpaceRespond.java
@@ -0,0 +1,25 @@
+package org.hibernate;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class SpaceRespond {
+    private static final String API_KEY = "c6ba5ab0fcb6f46d2d9ef4e5f084dbf8";  // Replace with your API key
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    public static SpaceRespond getWeather(double lat, double lon) throws Exception {
+        String url = String.format("%s?lat=%f&lon=%f&appid=%s", BASE_URL, lat, lon, API_KEY);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return  null;
+    }
+}
